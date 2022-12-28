17b35ec6a93353b7d0fc8dfbda727e8d5c0cb3c4
vloss <viniciusvianaloss@gmail.com>
2022-12-28 20:46:33 -0300

    Finalização de tela de temperaturas.

--- Conversores/ViewController.swift
+++ Conversores/ViewController.swift
@@ -9,13 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var tf_nr_temperatura: UITextField!
+    @IBOutlet weak var tf_nr_valor: UITextField!
+    
+    @IBOutlet weak var btn_farenheint: UIButton!
+    @IBOutlet weak var btn_celsius: UIButton!
+    
+    @IBOutlet weak var lb_resultado: UILabel!
+    @IBOutlet weak var lb_unidade_medida: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btn_next(_ sender: UIButton) {
+    }
+    
+    @IBAction func btn_converter(_ sender: UIButton) {
+    }
 }
 
